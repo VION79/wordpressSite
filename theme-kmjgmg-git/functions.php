cb378c6550cddf586614261c000bc32bc2b2555a
VION79 <vion79@gmail.com>
2023-02-03 13:52:45 -0500

    restructure

--- theme-kmjgmg-git/functions.php
+++ theme-kmjgmg-git/functions.php
@@ -2,7 +2,7 @@
 
 function followandrew_register_styles(){
 
-    wp_enqueue_style('followandrew-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('followandrew-style', get_template_directory_uri(), array('followandrew-bootstrap'), '1.0', 'all');
 }
 
  add_action( 'wp_enqueue_style', 'followandrew_register_styles'); 
