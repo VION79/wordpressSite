0ec92b55af7ff3edd30175b9ea341c8e26f8755c
VION79 <vion79@gmail.com>
2023-03-21 11:43:24 -0400

    change to build after npm run build

--- wpContentGithub/plugins/multi-columns/src/edit.js
+++ wpContentGithub/plugins/multi-columns/src/edit.js
@@ -32,7 +32,7 @@ import './editor.scss';
 export default function Edit() {
 	return (
 		<p { ...useBlockProps() }>
-			{ __( 'Multi Columns – hello from the editor!', 'multi-columns' ) }
+			{ __( 'VION79 Multi Columns – hello from the editor!', 'multi-columns' ) }
 		</p>
 	);
 }
