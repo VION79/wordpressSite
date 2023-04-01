5a744825f48dfcbbf52f247c3ddfd0f985984d91
VION79 <vion79@gmail.com>
2023-04-01 12:21:34 -0400

    addStyleVariations

--- wpContentGithub/plugins/multi-columns/build/index.asset.php
+++ wpContentGithub/plugins/multi-columns/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ad3996a38b9c62cc3f03');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b3843159812a6a7e7b4e');
