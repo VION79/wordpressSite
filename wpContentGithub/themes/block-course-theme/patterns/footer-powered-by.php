8e6f12444ac44aa85c73bb9b772ad35adcb607a5
VION79 <vion79@gmail.com>
2023-04-17 12:34:59 -0400

    i18nInBlockTheme

---
+++ wpContentGithub/themes/block-course-theme/patterns/footer-powered-by.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Powered by pattern
+ * Slug: block-course-theme/powered-by
+ */
+?>
+<!-- wp:paragraph {"align":"right"} -->
+    <p class="has-text-align-right"><?php echo __('Proudly powered by', 'blockcoursetheme'); ?><a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+<!-- /wp:paragraph -->
