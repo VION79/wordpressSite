cbc75feec0a3eae70084212d2684427c9b9c7583
VION79 <vion79@gmail.com>
2023-04-19 12:58:10 -0400

    lockingBlocksToPreventUserChanges

---
+++ wpContentGithub/themes/block-course-theme/patterns/happiness-bar-content-only.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Locked-Content-Only
+ * Slug: block-course-theme/locked-content-only
+ * Categories: featured, block-course-theme
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","align":"full","style":{"spacing":{"blockGap":"32px","padding":{"top":"128px","bottom":"128px"}}},"backgroundColor":"vivid-purple","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-vivid-purple-background-color has-text-color has-background" style="padding-top:128px;padding-bottom:128px"><!-- wp:heading {"textColor":"base"} -->
+<h2 class="has-base-color has-text-color">Content only happiness bar</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This pattern is a group of locked blocks. Users can only change the content and cannot change the design.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">See Schedule</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
---
+++ wpContentGithub/themes/block-course-theme/patterns/happiness-bar-pre-locked.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Pre-locked
+ * Slug: block-course-theme/pre-locked
+ * Categories: featured, block-course-theme
+ */
+?>
+
+<!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"align":"full","style":{"spacing":{"blockGap":"32px","padding":{"top":"128px","bottom":"128px"}}},"backgroundColor":"vivid-purple","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-vivid-purple-background-color has-text-color has-background" style="padding-top:128px;padding-bottom:128px"><!-- wp:heading {"textColor":"base"} -->
+<h2 class="has-base-color has-text-color">Pre-locked Happiness Bar</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This pattern is a group of pre-locked blocks. Users can unlock using the editor</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">See Schedule</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
