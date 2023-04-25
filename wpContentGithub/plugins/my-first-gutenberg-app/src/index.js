dbdc121833dfc83814b9c9d645bc187e10108464
VION79 <vion79@gmail.com>
2023-04-25 12:13:29 -0400

    retrieveWordpressDataRecord2

--- wpContentGithub/plugins/my-first-gutenberg-app/src/index.js
+++ wpContentGithub/plugins/my-first-gutenberg-app/src/index.js
@@ -1,30 +1,47 @@
-import { render } from '@wordpress/element';
-
- 
+import { SearchControl } from '@wordpress/components';
+import { useState, render } from '@wordpress/element'; 
 import { useSelect } from '@wordpress/data';
 import { store as coreDataStore } from '@wordpress/core-data';
 import { decodeEntities } from '@wordpress/html-entities';
   
 function MyFirstApp() {
+    const [searchTerm, setSearchTerm] = useState( '' );
     const pages = useSelect(
         select =>
             select( coreDataStore ).getEntityRecords( 'postType', 'page' ),
         []
     );
-    return <PagesList pages={ pages }/>;
-}
-  
-function PagesList( { pages } ) {
     return (
-        <ul>
-            { pages?.map( page => (
-                <li key={ page.id }>
-                    { decodeEntities( page.title.rendered ) }
-                </li>
-            ) ) }
-        </ul>
+        <div>
+            <SearchControl
+                onChange={ setSearchTerm }
+                value={ searchTerm }
+            />
+            <PagesList pages={ pages }/>
+        </div>
     )
 }
+
+
+function PagesList( { pages } ) {
+    return (
+        <table className="wp-list-table widefat fixed striped table-view-list">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                { pages?.map( page => (
+                    <tr key={ page.id }>
+                        <td>{ decodeEntities( page.title.rendered ) }</td>
+                    </tr>
+                ) ) }
+            </tbody>
+        </table>
+    )
+}
+
 
  
 window.addEventListener(
